f68793890852b98575e8395495d30c2a130b62bc
Acchisachi <acchi4843@gmail.com>
2023-02-20 23:30:31 +0530

    Social Media Commit

---
+++ reactProject/src/components/Rightbar.jsx
@@ -0,0 +1,12 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+const Rightbar = () => {
+  return (
+    <Box bgcolor={"red"} flex={1}>
+        <h2>Right Bar</h2>
+    </Box>
+  )
+}
+
+export default Rightbar
